12a51869e0a49166ecc252e0170cc9b6235610c5
Baptiste Busch <baptiste.busch@gmail.com>
2019-02-28 11:08:16 +0100

    Add camera handling

--- aruco_markers/run.sh
+++ aruco_markers/run.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-if [ $# -eq 0 ] ; then
-    echo 'Specifiy the camera id to use as it appears in /dev/video*:'
-    exit 0
-fi
-
-CAMID=$1
-#!/bin/bash
 NAME=$(echo "${PWD##*/}" | tr _ -)
 
-mkdir markers
-mkdir config
+mkdir -p markers
+mkdir -p config
 
 docker volume create --driver local \
     --opt type=none \
@@ -24,13 +17,13 @@ docker volume create --driver local \
     config_vol
 
 xhost +
-sudo docker run \
+docker run \
+    --privileged \
 	--net=host \
 	-it \
-	--env="DISPLAY" \
+	--env DISPLAY=${DISPLAY} \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--volume="markers_vol:/home/ros/ros_ws/src/aruco_markers/markers/:rw" \
 	--volume="config_vol:/home/ros/ros_ws/src/aruco_markers/config/:rw" \
-    --privileged \
-    --device=/dev/video${CAMID}:/dev/video${CAMID} \
+	--volume="/dev/video0:/dev/video0" \
 	$NAME:latest
