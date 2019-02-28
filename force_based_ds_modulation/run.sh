95260fe1497077a655d3dd7e60b31210ec4cbfc9
Baptiste Busch <baptiste.busch@gmail.com>
2019-02-28 09:46:59 +0100

    Create a shared volume to store the log generated files

--- force_based_ds_modulation/run.sh
+++ force_based_ds_modulation/run.sh
@@ -6,8 +6,17 @@ if [ $# -eq 0 ] ; then
     exit 0
 fi
 
+mkdir data
+
 NAME=$(echo "${PWD##*/}" | tr _ -)
 TAG=$(echo "$1" | tr _/ -)
+
+# create a shared volume to store the generate logs
+docker volume create --driver local \
+    --opt type=none \
+    --opt device=$PWD/data/ \
+    --opt o=bind \
+    data_vol
 
 xhost +
 docker run \
@@ -17,6 +26,7 @@ docker run \
 	-it \
 	--env="DISPLAY" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--volume="data_vol:/home/ros/ros_ws/src/force_based_ds_modulation/data:rw" \
     --device /dev/bus/usb:/dev/bus/usb:rwm \
     --env ROS_MASTER_URI=http://192.168.0.1:11311 \
     --env ROS_IP=192.168.0.1 \
