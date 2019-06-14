59d4ef95b9f36f663be733e55014a82ad9244526
Baptiste Busch <baptiste.busch@gmail.com>
2019-06-14 08:19:02 +0200

    Docker image for the programming test

---
+++ programming_test/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+NAME=$(echo "${PWD##*/}" | tr _ -)
+
+mkdir -p ros_ws
+
+# create a shared volume to store the ros_ws
+docker volume create --driver local \
+    --opt type=none \
+    --opt device=$PWD/ros_ws/ \
+    --opt o=bind \
+    ${NAME}_ros_ws_vol
+
+xhost +
+docker run \
+    --privileged \
+    --runtime=nvidia \
+    --net=host \
+    -it \
+    --env DISPLAY=${DISPLAY} \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --volume="${NAME}_ros_ws_vol:/home/ros/ros_ws/src:rw" \
+    $NAME:latest
