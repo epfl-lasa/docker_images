12cf75a374ec457a4ad172aa833482c61aab6b3a
Baptiste Busch <baptiste@aica.tech>
2019-06-27 10:52:50 +0200

    Update ros2 docker to include graphics and nightly build

--- ros2_setup/run.sh
+++ ros2_setup/run.sh
@@ -1,8 +1,7 @@
-mkdir -p ros2_ws
-mkdir -p ros2_overlay_ws
-
 NAME=$(echo "${PWD##*/}" | tr _ -)
 TAG=$(echo "$1" | tr _/ -)
+
+mkdir -p ros2_ws
 
 # create a shared volume to store the ros_ws
 docker volume create --driver local \
@@ -11,19 +10,15 @@ docker volume create --driver local \
     --opt o=bind \
     ros2_setup_ros2_ws_vol
 
-# create a shared volume to store the ros_ws
-docker volume create --driver local \
-    --opt type=none \
-    --opt device=$PWD/ros2_overlay_ws/ \
-    --opt o=bind \
-    ros2_setup_ros2_overlay_ws_vol
-
 xhost +
 docker run \
+    --runtime=nvidia \
+    --privileged \
 	--net=host \
 	-it \
-	--env="DISPLAY" \
+	--env DISPLAY=${DISPLAY} \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
 	--volume="ros2_setup_ros2_ws_vol:/home/ros2/ros2_ws/:rw" \
-    --volume="ros2_setup_ros2_overlay_ws_vol:/home/ros2/ros2_overlay_ws/:rw" \
 	$NAME:latest
