48277acc188599c636069fce77f86439f1ff53c2
Lukas Huber <hubernikus@gmail.com>
2019-08-09 14:33:09 +0200

    update franka file

--- franka_ros/run.sh
+++ franka_ros/run.sh
@@ -1,22 +1,11 @@
-#!/bin/bash
-if [ $# -eq 0 ] ; then
-    echo 'Specifiy the branch to build the image with among:'
-    echo '- master'
-    echo '- dev_controllers'
-    exit 0
-fi
-
 NAME=$(echo "${PWD##*/}" | tr _ -)
 TAG=$(echo "$1" | tr _/ -)
 
 xhost +
 docker run \
-	--ulimit rtprio=95:95 \
-	--ulimit memlock=-1:-1 \
+	--privileged \
 	--net=host \
 	-it \
 	--env="DISPLAY" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --env ROS_MASTER_URI=http://192.168.0.1:11311 \
-    --env ROS_IP=192.168.0.1 \
-	$NAME:$TAG
+    $NAME:$TAG
