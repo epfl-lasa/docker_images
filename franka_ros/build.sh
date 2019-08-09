5ad24b3b133f0437276aaa91d928199185c9fd26
Baptiste Busch <baptiste.busch@gmail.com>
2019-08-09 14:27:41 +0200

    Modify build.sh with new features

--- franka_ros/build.sh
+++ franka_ros/build.sh
@@ -1,3 +1,26 @@
+REBUILD=0
+
+while getopts 'r' opt; do
+    case $opt in
+        r) REBUILD=1 ;;
+        *) echo 'Error in command line parsing' >&2
+           exit 1
+    esac
+done
+shift "$(( OPTIND - 1 ))"
+
 NAME=$(echo "${PWD##*/}" | tr _ -)
 TAG=$(echo "$1" | tr _/ -)
-docker build -t $NAME:$TAG .
+
+if [ -z "$TAG" ]; then
+	TAG="latest"
+fi
+
+if [ "$REBUILD" -eq 1 ]; then
+	docker build \
+    	--no-cache \
+ 		-t $NAME:$TAG .
+ else
+	docker build \
+ 		-t $NAME:$TAG .
+ fi
