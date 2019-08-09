7e73316abf477e6f2dea10e9cefefd90e191e9e5
Baptiste Busch <baptiste.busch@gmail.com>
2019-08-09 14:34:58 +0200

    Add ros workspace if needed

--- franka_ros/run.sh
+++ franka_ros/run.sh
@@ -1,11 +1,27 @@
 NAME=$(echo "${PWD##*/}" | tr _ -)
 TAG=$(echo "$1" | tr _/ -)
 
+if [ -z "$TAG" ]; then
+	TAG="latest"
+fi
+
+mkdir -p ros_ws
+
+# create a shared volume to store the ros_ws
+docker volume create --driver local \
+    --opt type=none \
+    --opt device=$PWD/ros_ws/ \
+    --opt o=bind \
+    ${NAME}_ros_ws_vol
+
 xhost +
 docker run \
-	--privileged \
+    --privileged \
 	--net=host \
 	-it \
-	--env="DISPLAY" \
+	--env DISPLAY=${DISPLAY} \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    $NAME:$TAG
+	--env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+	--volume="${NAME}_ros_ws_vol:/home/ros/ros_ws/:rw" \
+	${NAME}:${TAG}
