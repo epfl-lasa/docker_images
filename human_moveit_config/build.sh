9fc4825890aa27022f02a03bbac146de195233be
Baptiste Busch <baptiste.busch@epfl.ch>
2019-08-22 17:34:27 +0200

    Include desktop full instead of small images

--- human_moveit_config/build.sh
+++ human_moveit_config/build.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+REBUILD=0
+
+while getopts 'r' opt; do
+    case $opt in
+        r) REBUILD=1 ;;
+        *) echo 'Error in command line parsing' >&2
+           exit 1
+    esac
+done
+shift "$(( OPTIND - 1 ))"
+
 NAME=$(echo "${PWD##*/}" | tr _ -)
-docker build --build-arg NB_CORE=8 -t $NAME:latest .
+
+if [ "$REBUILD" -eq 1 ]; then
+	docker build \
+    	--no-cache \
+ 		-t ${NAME}:latest .
+ else
+	docker build \
+ 		-t ${NAME}:latest .
+ fi
--- ros_setup/build.sh
+++ ros_setup/build.sh
@@ -1,19 +1,39 @@
-#!/bin/bash
-if [ $# -eq 0 ] ; then
-    echo 'Specifiy the ros distro to build the image on among:'
-    echo '- melodic-ros-base'
-    echo '- kinetic-ros-base'
-    echo '- indigo-ros-base'
-    echo '- other available on ros docker hub (not tested)'
-    exit 0
-fi
+BASE_IMAGE=osrf/ros
+BASE_TAG=indigo-desktop-full
+
+docker pull ${BASE_IMAGE}:${BASE_TAG}
+
+cp ../ssh_keys/ssh_key config/
+
+REBUILD=0
+
+while getopts 'r' opt; do
+    case $opt in
+        r) REBUILD=1 ;;
+        *) echo 'Error in command line parsing' >&2
+           exit 1
+    esac
+done
+shift "$(( OPTIND - 1 ))"
 
 NAME=$(echo "${PWD##*/}" | tr _ -)
-TAG=$(echo "$1" | tr _/ -)
 
 UID="$(id -u $USER)"
 GID="$(id -g $USER)"
-docker build --build-arg ROS_DISTRO=$1 \
- 	--build-arg UID=$UID \
- 	--build-arg GID=$GID \
- 	-t $NAME:$TAG .
+
+if [ "$REBUILD" -eq 1 ]; then
+	docker build \
+    	--no-cache \
+    	--build-arg BASE_IMAGE=${BASE_IMAGE} \
+    	--build-arg BASE_TAG=${BASE_TAG} \
+ 		--build-arg UID=${UID} \
+ 		--build-arg GID=${GID} \
+ 		-t ${NAME}:${BASE_TAG} .
+ else
+	docker build \
+	    --build-arg BASE_IMAGE=${BASE_IMAGE} \
+	    --build-arg BASE_TAG=${BASE_TAG} \
+ 		--build-arg UID=${UID} \
+ 		--build-arg GID=${GID} \
+ 		-t ${NAME}:${BASE_TAG} .
+ fi
