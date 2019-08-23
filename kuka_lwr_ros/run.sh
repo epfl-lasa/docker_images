68409b5c98abfbc01c79268084c0a06595fd8114
Baptiste Busch <baptiste.busch@epfl.ch>
2019-08-23 15:15:26 +0200

    Use kinetic branch (indigo is not supported anymore)

--- kuka_lwr_ros/run.sh
+++ kuka_lwr_ros/run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-if [ $# -eq 0 ] ; then
-    echo 'Specifiy the branch to build the image with among:'
-    echo '- master'
-    echo '- dev_controllers'
-    exit 0
-fi
-
 NAME=$(echo "${PWD##*/}" | tr _ -)
-TAG=$(echo "$1" | tr _/ -)
+TAG=master-ros-kinetic-walid
 
 xhost +
 docker run \
