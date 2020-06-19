5dedc5dae50e0e013e6db20096d44b906313a2a9
Gustav_Vision_PC <gustav.henriks@telia.com>
2020-06-19 12:48:27 +0200

    .

--- realsense_test/run.sh
+++ realsense_test/run.sh
@@ -1,25 +1,16 @@
 NAME=$(echo "${PWD##*/}" | tr _ -)
 TAG=$(echo "$1" | tr _/ -)
 
-mkdir -p ros2_ws
-
-# create a shared volume to store the ros_ws
-docker volume create --driver local \
-    --opt type=none \
-    --opt device=$PWD/ros2_ws/ \
-    --opt o=bind \
-    ${NAME}_ros2_ws_vol
-
 xhost +
 docker run \
     --privileged \
 	--net=host \
 	-it \
+	--rm \
 	--env DISPLAY=${DISPLAY} \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--env="XAUTHORITY=$XAUTH" \
     --volume="$XAUTH:$XAUTH" \
-	--volume="${NAME}_ros2_ws_vol:/home/ros2/ros2_ws/:rw" \
 	--device=/dev/dri:/dev/dri \
 	-v /dev/video \
 	$NAME:latest
